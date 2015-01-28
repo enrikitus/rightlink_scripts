dad2be44b38008ffaab21e89db4862cdd1a09460
Enric Trepat <enric.casadevall@rightscale.com>
2015-01-28 13:18:24 +0100

    OPS-169 - rightscripts_into_github_test

--- rll/metadata.rb
+++ rll/metadata.rb
@@ -21,7 +21,8 @@ attribute   "HOSTNAME",
   :required => "optional",
   :type => "string",
   :default => "env:RS_SERVER_NAME",
-  :recipes => ["rll::init","rll::SYS_set_hostname"]
+  #:recipes => ["rll::init","rll::SYS_set_hostname"]
+  :recipes => ["rll::SYS_set_hostname"]
 
 attribute   "COLLECTD_SERVER",
   :display_name => "RightScale monitoring server to send data to",
